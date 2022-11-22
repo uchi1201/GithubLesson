6845e30a6213a04bea14e68810800e6a7ea535f5
Nakayama, Ryuichi <rnakayama@dxc.com>
2022-11-22 14:23:34 +0900

    コメント追加

--- app/src/main/java/com/jeis/githublesson/MainActivity.kt
+++ app/src/main/java/com/jeis/githublesson/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 2022/11/22 コメント追加 nakayama
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
